342b6d5694d37cd113301dc1ac499a64c856a440
Mouad Haikal <Mouad.Haikal@um6p.ma>
2025-10-05 22:01:20 +0100

    Exercise 3

--- src/main/java/samplearrays/BankAccount.java
+++ src/main/java/samplearrays/BankAccount.java
@@ -1,33 +1,70 @@
 package samplearrays;
 
 public class BankAccount {
-
     String name;
     double currentBalance;
-    //TO-DO: Initialize an Array with 1000 in size that stores Double called 'transactions' to keep track of the user's transactions
+    private Double[] transactions;
+    private int transactionId;
 
     public BankAccount(String name, int startingBalance){
+        this.name = name;
+        this.currentBalance = startingBalance;
 
+        this.transactionId = 0;
+        this.transactions = new Double[1000];
     }
 
     public void deposit(double amount){
+        if(amount < 0) {
+            System.out.println("Invalid amount");
+            return;
+        }
 
+        System.out.println(name + " has deposited an amount of " + amount);
+        System.out.println("Previous balance: " + currentBalance);
+
+        transactions[transactionId] = amount;
+        transactionId++;
+
+        currentBalance += amount;
+
+        System.out.println("Current balance: " + currentBalance );
     }
 
     public void withdraw(double amount){
+        if(amount < 0 ) {
+            System.out.println("Invalid amount");
+            return;
+        }
+        if(amount > currentBalance) {
+            System.out.println("Insufficient balance");
+            return;
+        }
 
+        System.out.println(name + " has withdrawn an amount of " + amount);
+        System.out.println("Previous balance: " + currentBalance);
+
+        transactions[transactionId] = -amount;
+        transactionId++;
+
+        currentBalance -= amount;
+
+        System.out.println("Current balance: " + currentBalance );
     }
 
     public void displayTransactions(){
+        System.out.println("Transactions History:");
 
+        for(int i = 0; i < transactionId; i++) {
+            System.out.println("Transaction " + i + ": " + transactions[i]);
+        }
     }
 
     public void displayBalance(){
-
+        System.out.println("Current balance: " + currentBalance);
     }
 
     public static void main(String[] args) {
-
         BankAccount john = new BankAccount("John Doe", 100);
 
         // ----- DO NOT CHANGE -----
@@ -43,7 +80,5 @@ public class BankAccount {
         john.displayBalance();
 
         // ----- DO NOT CHANGE -----
-
     }
-
 }
